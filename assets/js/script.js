2096e6a658c81d067e8e8440049f89f4ca54c3cc
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-01-01 21:09:28 -0700

    re-organize code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +11,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
